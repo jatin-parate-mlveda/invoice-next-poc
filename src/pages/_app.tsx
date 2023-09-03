72ed7fadf362c86bc10d75a272ce36c546ca9574
Jatin <jatin.parate@mlveda.com>
2023-09-03 23:24:45 +0530

    Fix error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,38 +1,6 @@
-import '@/styles/globals.css';
-import type { AppProps } from 'next/app';
-import { Provider as AppBridgeProvider } from '@shopify/app-bridge-react';
-import { AppProvider } from '@shopify/polaris';
-import enTranslations from '@shopify/polaris/locales/en.json';
-import AppQueryClientProvider from '@/contexts/react-query.context';
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  if (typeof window === 'undefined') {
-    const { getLoadingScreen } = Component as any;
-    if (getLoadingScreen) {
-      const LoadingScreen = getLoadingScreen();
-      return (
-        <AppProvider i18n={enTranslations}>
-          <LoadingScreen />
-        </AppProvider>
-      );
-    } else {
-      return <h1>Loading...</h1>;
-    }
-  }
-
-  return (
-    <AppQueryClientProvider>
-      <AppBridgeProvider
-        config={{
-          forceRedirect: true,
-          host: new URLSearchParams(window.location.search).get('host')!,
-          apiKey: process.env.NEXT_PUBLIC_SHOPIFY_API_KEY!,
-        }}
-      >
-        <AppProvider i18n={enTranslations}>
-          <Component {...pageProps} />
-        </AppProvider>
-      </AppBridgeProvider>
-    </AppQueryClientProvider>
-  );
+  return <Component {...pageProps} />;
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,37 @@
-import DashboardLoadingCom from '@/components/home/LoadingScreen';
-import { Suspense } from 'react';
-import DashBoardPage from '@/components/home/Index';
+import DashboardLoadingCom from "@/components/home/LoadingScreen";
+import { Provider as AppBridgeProvider } from "@shopify/app-bridge-react";
+import { Suspense } from "react";
+import DashBoardPage from "@/components/home/Index";
+import enTranslations from "@shopify/polaris/locales/en.json";
+import { AppProvider } from "@shopify/polaris";
+import AppQueryClientProvider from "@/contexts/react-query.context";
 
-export default function DashboardPage() {
+function DashboardPage() {
+  if (typeof window === "undefined") {
+    return (
+      <AppProvider i18n={enTranslations}>
+        <DashboardLoadingCom />
+      </AppProvider>
+    );
+  }
+
   return (
-    <Suspense fallback={<DashboardLoadingCom />}>
-      <DashBoardPage />
-    </Suspense>
+    <AppQueryClientProvider>
+      <AppBridgeProvider
+        config={{
+          forceRedirect: true,
+          host: new URLSearchParams(window.location.search).get("host")!,
+          apiKey: process.env.NEXT_PUBLIC_SHOPIFY_API_KEY!,
+        }}
+      >
+        <AppProvider i18n={enTranslations}>
+          <Suspense fallback={<DashboardLoadingCom />}>
+            <DashBoardPage />
+          </Suspense>
+        </AppProvider>
+      </AppBridgeProvider>
+    </AppQueryClientProvider>
   );
 }
 
-DashboardPage.getLoadingScreen = () => <DashboardLoadingCom />;
+export default DashboardPage;
