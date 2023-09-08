54948ef9bbc2f69df9c7cf57136932a887524df9
Jatin <jatin.parate@mlveda.com>
2023-09-08 21:24:24 +0530

    disable auth temporarily

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,16 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-import { useReportWebVitals } from "next/web-vitals";
+import '@/styles/globals.css';
+import { AppProvider } from '@shopify/polaris';
+import type { AppProps } from 'next/app';
+import { useReportWebVitals } from 'next/web-vitals';
+import enTranslations from '@shopify/polaris/locales/en.json';
 
 export default function App({ Component, pageProps }: AppProps) {
-  useReportWebVitals((metric) => {
+  useReportWebVitals(metric => {
     console.log(metric);
   });
-  return <Component {...pageProps} />;
+  return (
+    <AppProvider i18n={enTranslations}>
+      <Component {...pageProps} />
+    </AppProvider>
+  );
 }
