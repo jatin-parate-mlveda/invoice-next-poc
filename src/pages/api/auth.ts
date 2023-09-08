e2734f28920d71c1a4f761bab67d952cc66cc249
Jatin <jatin.parate@mlveda.com>
2023-09-08 21:31:15 +0530

    disable auth temporarily

--- src/pages/api/auth.ts
+++ src/pages/api/auth.ts
@@ -4,35 +4,37 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 export default async function auth(req: NextApiRequest, res: NextApiResponse) {
   const searchParams = new URL('https://localhost' + req.url!).searchParams;
   const shopName = req.query['shop'] as string;
+  res.redirect(`/?${searchParams.toString()}`);
+  return;
 
-  const apiRes = await fetch(
-    `${process.env.API_ENDPOINT}api/shop/app-exists?shop=${shopName}`,
-  );
-  const {
-    isInstalled,
-    isOldIhUser,
-    // stepReached,
-    isSetupCompleted,
-  } = await apiRes.json();
+  // const apiRes = await fetch(
+  //   `${process.env.API_ENDPOINT}api/shop/app-exists?shop=${shopName}`,
+  // );
+  // const {
+  //   isInstalled,
+  //   isOldIhUser,
+  //   // stepReached,
+  //   isSetupCompleted,
+  // } = await apiRes.json();
 
-  if (!isInstalled) {
-    if (!isOldIhUser) {
-      res.redirect(
-        `${process.env.API_ENDPOINT}shopify?${searchParams.toString()}`,
-      );
-      return;
-    }
-  }
+  // if (!isInstalled) {
+  //   if (!isOldIhUser) {
+  //     res.redirect(
+  //       `${process.env.API_ENDPOINT}shopify?${searchParams.toString()}`,
+  //     );
+  //     return;
+  //   }
+  // }
 
-  if (
-    !isOldIhUser &&
-    !req.url!.startsWith('/admin-links') &&
-    !isSetupCompleted
-  ) {
-    res.redirect(`/?${searchParams.toString()}`);
-    // res.redirect(`/welcome?${searchParams.toString()}`);
-    return;
-  } else {
-    res.redirect(`/?${searchParams.toString()}`);
-  }
+  // if (
+  //   !isOldIhUser &&
+  //   !req.url!.startsWith('/admin-links') &&
+  //   !isSetupCompleted
+  // ) {
+  //   res.redirect(`/?${searchParams.toString()}`);
+  //   // res.redirect(`/welcome?${searchParams.toString()}`);
+  //   return;
+  // } else {
+  //   res.redirect(`/?${searchParams.toString()}`);
+  // }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,15 @@
-import DashboardLoadingCom from "@/components/home/LoadingScreen";
-import { Provider as AppBridgeProvider } from "@shopify/app-bridge-react";
-import { Suspense } from "react";
+import DashboardLoadingCom from '@/components/home/LoadingScreen';
+import { Provider as AppBridgeProvider } from '@shopify/app-bridge-react';
+import { Suspense } from 'react';
 // import DashBoardPage from "@/components/home/Index";
-import { AppProvider } from "@shopify/polaris";
-import AppQueryClientProvider from "@/contexts/react-query.context";
-import dynamic from "next/dynamic";
+import AppQueryClientProvider from '@/contexts/react-query.context';
+import dynamic from 'next/dynamic';
 
 const DashboardPageDynamic = dynamic(() => import('@/components/home/Index'));
 
 function DashboardPage() {
-  if (typeof window === "undefined") {
-    return (
-      <AppProvider i18n={{}}>
-        <DashboardLoadingCom />
-      </AppProvider>
-    );
+  if (typeof window === 'undefined') {
+    return <DashboardLoadingCom />;
   }
 
   return (
@@ -22,15 +17,13 @@ function DashboardPage() {
       <AppBridgeProvider
         config={{
           forceRedirect: true,
-          host: new URLSearchParams(window.location.search).get("host")!,
+          host: new URLSearchParams(window.location.search).get('host')!,
           apiKey: process.env.NEXT_PUBLIC_SHOPIFY_API_KEY!,
         }}
       >
-        <AppProvider i18n={{}}>
-          <Suspense fallback={<DashboardLoadingCom />}>
-            <DashboardPageDynamic />
-          </Suspense>
-        </AppProvider>
+        <Suspense fallback={<DashboardLoadingCom />}>
+          <DashboardPageDynamic />
+        </Suspense>
       </AppBridgeProvider>
     </AppQueryClientProvider>
   );
