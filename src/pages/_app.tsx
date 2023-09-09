c4e11022c4f758340ae1b7dde195e52bd53cd9c2
Jatin <jatin.parate@mlveda.com>
2023-09-09 19:19:54 +0530

    use crisp from cdn instead of sdk

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,11 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import dynamic from 'next/dynamic';
+// import dynamic from 'next/dynamic';
 import { useReportWebVitals } from 'next/web-vitals';
 
-const CrispWithNoSSR = dynamic(() => import('../components/CrispChat'), {
-  ssr: false,
-});
+// const CrispWithNoSSR = dynamic(() => import('../components/CrispChat'), {
+//   ssr: false,
+// });
 
 export default function App({ Component, pageProps }: AppProps) {
   useReportWebVitals(metric => {
@@ -14,7 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <>
-      <CrispWithNoSSR />
+      {/* <CrispWithNoSSR /> */}
       <Component {...pageProps} />
     </>
   );
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
@@ -7,6 +8,25 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <Script
+          id='crisp'
+          strategy='beforeInteractive'
+          type='text/javascript'
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.$crisp = [];
+              window.CRISP_WEBSITE_ID = '9a70b14e-b8db-48b2-8773-95eb66b91cee';
+              (function IIF() {
+                const d = document;
+                const s = d.createElement('script');
+                s.src = 'https://client.crisp.chat/l.js';
+                d.getElementsByTagName('head')[0].appendChild(s);
+                window.$crisp.push(['set', 'session:segments', [['invoicehero']]]);
+                window.$crisp.push(['set', 'session:data', ['storename', new URLSearchParams(window.location.search).get('shop')]]);
+              })();
+            `,
+            }}
+          />
       </body>
     </Html>
   );
